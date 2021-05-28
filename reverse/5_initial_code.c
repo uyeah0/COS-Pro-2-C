843678a5481a3f28bbcbbf383723c1fe13e0c689
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-28 23:32:57 +0900

    Update 5_initial_code.c

--- reverse/5_initial_code.c
+++ reverse/5_initial_code.c
@@ -5,7 +5,7 @@
 int* solution(int arr[], int arr_len) {
     int left = 0;
     int right = arr_len - 1;
-    while ( left != right ) {
+    while ( left < right ) {
         int temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
