cc1c2bdaf7d1204e48d392891e54e0e475794628
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-06-09 00:13:22 +0900

    Update and rename shirt_size/1_initial_code.c to sec01/shirt_size/1_initial_code.c

--- sec01/shirt_size/1_initial_code.c
+++ sec01/shirt_size/1_initial_code.c
@@ -1,4 +1,3 @@
-// You may use include as below.
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -20,13 +19,12 @@ int* solution(const char* shirt_size[], int shirt_size_len) {
     return answer;
 }
 
-// The following is main function to output testcase.
+
 int main() {
     char* shirt_size[] = { "XS", "S", "L", "L", "XL", "S" };
     int shirt_size_len = 6;
     int* ret = solution(shirt_size, shirt_size_len);
 
-    // Press Run button to receive output.       
     printf("Solution: return value of the function is {");
     for (int i = 0; i < 6; i++) {
         if (i != 0) printf(", ");
