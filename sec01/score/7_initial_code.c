cafbc6f85a7e111d60a16700ea7af464d853873a
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-06-09 00:10:54 +0900

    Update 7_initial_code.c

--- sec01/score/7_initial_code.c
+++ sec01/score/7_initial_code.c
@@ -10,12 +10,10 @@ int solution(int scores[], int scores_len) {
     return count;
 }
 
-// The following is main function to output testcase. The main function is correct and you shall correct solution function.
 int main() {
     int scores[10] = { 650, 722, 914, 558, 714, 803, 650, 679, 669, 800 };
     int scores_len = 10;
     int ret = solution(scores, scores_len);
 
-    // Press Run button to receive output.
     printf("Solution: return value of the function is %d .\n", ret);
 }
